cfae4ca8c0ac249f7dba367b1b281f21e427e643
Michael Gao <michaelgao2004@gmail.com>
2025-12-02 00:24:46 -0700

    started kdocs

--- LottoEvent/app/src/main/java/com/example/lottoevent/models/Event.kt
+++ LottoEvent/app/src/main/java/com/example/lottoevent/models/Event.kt
@@ -1,5 +1,21 @@
 package com.example.lottoevent.models
 
+/**
+ * An event. Holds details about a lottery-style event, including the organizer,
+ * participant capacity, and a list of users currently on the waiting list.
+ *
+ * @constructor Creates an Event object with default or specified values.
+ * @property id A unique identifier for the event.
+ * @property title The name of the event.
+ * @property description A description of the event.
+ * @property capacity The maximum number of attendees.
+ * @property organizer The [User] who created the event.
+ * @property waitingList The [WaitingList] managing users waiting to attend.
+ * @author Michael
+ * @since 1.0.0
+ * @see [User]
+ * @see [WaitingList]
+ */
 data class Event(
     val id: String = "",
     var title: String = "",
